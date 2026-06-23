dfb20ba9ca375ab2cb9ce387e544512351e85029
Abdul Hamid <ah1260794@gmail.com>
2026-06-23 07:21:28 +0700

    fix urutan jadwal khotib

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -6,12 +6,23 @@ use App\Models\Jadwal;
 
 class LandingController extends Controller
 {
+
     public function index()
     {
-        $jadwals = Jadwal::orderBy('created_at', 'desc')->get();
 
-        return view('landing.index', [
-            'jadwals' => $jadwals
-        ]);
+
+        $jadwals = Jadwal::orderBy('tanggal','asc')
+            ->orderBy('id','asc')
+            ->get();
+
+
+
+        return view(
+            'landing.index',
+            compact('jadwals')
+        );
+
+
     }
+
 }
