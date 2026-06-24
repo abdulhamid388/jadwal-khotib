b184c9b58e7af4b3efac8b014cf3b0e88c133d76
Abdul Hamid <ah1260794@gmail.com>
2026-06-24 14:16:53 +0700

    update tampilan landing

--- resources/views/landing/layout.blade.php
+++ resources/views/landing/layout.blade.php
@@ -15,19 +15,17 @@ Jadwal Khotib Jumat
 
 
 
-<!-- CSS WEBSITE -->
-
-<link rel="stylesheet"
-href="{{ asset('landing/css/style.css') }}">
-
-
-
-<!-- BOOTSTRAP -->
+<!-- BOOTSTRAP DULU -->
 
 <link 
 href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
 rel="stylesheet">
 
+
+<!-- CSS KAMU TERAKHIR -->
+
+<link rel="stylesheet"
+href="{{ asset('landing/css/style.css') }}">
 
 
 <!-- FONT -->
